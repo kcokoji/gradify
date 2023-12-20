ce80cd9d0bfc88cfc901b1a271065ad935174e30
Okoji Kelechi <100976015+kcokoji@users.noreply.github.com>
2023-12-20 21:34:37 +0100

    Update Create page error

--- src/app/dashboard/create/page.js
+++ src/app/dashboard/create/page.js
@@ -107,7 +107,7 @@ export default function Example() {
                   <input
                     type="text"
                     name="semester-name"
-                    id="last-name"
+                    id="semester-name"
                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
                     required
                   />
@@ -250,5 +250,5 @@ export default function Example() {
           </form>
         
       
-  
+  );
 }
